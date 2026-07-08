6b1ca43a6faf34ca73c4cacd919329b54f84b07b
Jake Wang <jakew1576@gmail.com>
2026-07-08 02:29:39 -0700

    Center map onto LA

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Map } from "@/components/ui/map";
 export default function Home() {
   return (
     <main className="h-dvh w-full">
-      <Map />
+      <Map center={[-118.24, 34.05]} zoom={10}/>
     </main>
   );
 }
